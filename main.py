e5e43dabf299ba25bfc787241ce61d36835d3978
Nils Deckert <nils@deckert.dev>
2021-09-22 15:16:49 +0200

    Added setup for rss feed, manual loading of newest entry

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from discord_slash.utils.manage_commands import create_option
 from discord.ext.commands import has_permissions
 from dotenv import load_dotenv
 
+import feedparser
+import html2text
+
 from pytz import timezone
 from datetime import datetime, timedelta
 
@@ -255,5 +258,102 @@ try:
             await ctx.send("Error: Make sure you only use standard emojis or emojis from this server")
 except discord.ext.commands.errors.MissingPermissions:
     pass
+
+try:
+    @has_permissions(manage_roles=True)
+    @slash.slash(name="add_rss_feed", guild_ids=guild_ids, description="Setup a rss feed for this channel",
+                 options=[
+                     create_option(
+                         name="feed_link",
+                         description="link to rss feed",
+                         option_type=STRING,
+                         required=True
+                     ),
+                     create_option(
+                         name="role",
+                         description="role to ping on rss update",
+                         option_type=ROLE,
+                         required=False
+                     )
+                 ])
+    async def add_rss_feed(ctx: SlashContext, feed_link: str, role: discord.role = None):
+        guild_id = str(ctx.guild_id)
+        channel_id = str(ctx.channel.id)
+
+        if not config.has_section(guild_id):
+            config.add_section(str(guild_id))
+
+        if not config.has_option(guild_id, "rss_channels"):
+            config.set(guild_id, "rss_channels", channel_id)
+        else:
+            channels = config.get(guild_id, "rss_channels").split(",")
+            if channel_id not in channels:
+                config.set(guild_id, "rss_channels", f"{config.get(guild_id, 'rss_channels')},{channel_id}")
+
+        config.set(guild_id, f"{channel_id}_link", feed_link)
+        if role:
+            config.set(guild_id, f"{channel_id}_role", role.name)
+
+        with open('config.ini', 'w', encoding="utf-8") as f:
+            config.write(f)
+            logging.info(f"{guild_id}: Added rss feed for channel {channel_id}")
+        await ctx.send("Added new rss feed.", hidden=True)
+except discord.ext.commands.errors.MissingPermissions:
+    pass
+
+try:
+    @has_permissions(manage_roles=True)
+    @slash.slash(name="load_rss", guild_ids=guild_ids, description="Load newest rss entry",
+                 options=[
+                     create_option(
+                         name="ping",
+                         description="ping according role",
+                         option_type=BOOLEAN,
+                         required=False
+                     )
+                 ])
+    async def load_rss(ctx: SlashContext, ping=False):
+        guild_id = str(ctx.guild_id)
+        channel_id = str(ctx.channel.id)
+
+        if (not config.has_section(guild_id)) or (not config.has_option(guild_id, f"{channel_id}_link")):
+            await ctx.send("No rss feed has been setup yet", hidden=True)
+            return
+
+        if ping:
+            role = config.get(guild_id, f"{channel_id}_role")
+        else:
+            role = None
+
+        await send_rss_entry(int(channel_id), config.get(guild_id, f"{channel_id}_link"), role=role)
+
+except discord.ext.commands.errors.MissingPermissions:
+    pass
+
+
+# https://github.com/zenxr/discord_rss_bot
+async def send_rss_entry(channel_id: int, link: str, role: str = None):
+    d = feedparser.parse(link)
+    channel = client.get_channel(channel_id)
+
+    post = d.entries[0]
+    title = (post.title.encode('utf-8', 'ignore').decode('utf-'))
+    html = (post.summary.encode('utf-8', 'ignore').decode('utf-8'))
+    text = html2text.html2text(html)
+    link = post.link
+
+    if len(text) > 1024:
+        text = text[:1018] + "\n[...]"
+
+    embed = discord.Embed(title=title)
+    embed.add_field(name="Text: ", value=text, inline=False)
+    embed.add_field(name="Link: ", value=link, inline=False)
+
+
+    logging.info(f"New rss entry for channel {channel_id}")
+    await channel.send(embed=embed)
+    if role:
+        await channel.send(discord.utils.get(channel.guild.roles, name=role).mention)
+
 
 client.run(TOKEN)
